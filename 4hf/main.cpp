b2158b57a4743108bc8b04ca64223885ba9b5654
Abeldom <47552057+Abeldom@users.noreply.github.com>
2019-09-19 10:49:10 +0300

    Add files via upload

--- 4hf/main.cpp
+++ 4hf/main.cpp
@@ -7,38 +7,44 @@ int main()
 {
     ifstream in("input.txt");
     ofstream outpr("prim.txt");
-    ofstream outtok("tokeletes.txt");
-    ofstream outpall("pallindrom.out");
-    int a;
-    while(input>>x){
-        int y=x;
+    ofstream outtok("tokeletes.out");
+    ofstream outpall("pallindrom.dat");
+    int x,y,z,s,b;
+    bool prim;
+    while(in>>x){
 
 
-        int b=x/2;
-        bool prim=true;
-        int s=1;
+        b=x/2;
+        prim=true;
+        s=1;
 
-        for(int i=2;i<=b;i=i+2){
-            if(b%i==0){
-                prim=false;
-                s=s+i;
+        if(x>=2){
+            if(x==2){
+                outpr<<"2";
+            }else{
+                for( int i=2;i<=b;i++){
+                    if(x%i==0){
+                        prim=false;
+                        s=s+i;
+                    }
+                }
+                if(prim==true){
+                    outpr<<x<<" ";
+                }
+                if(s==x){
+                    outtok<<x<<" ";
+                }
             }
         }
-        if(x==1){
 
-        }
-        if(prim){
-            outpr<<x;
-        }
-
-
-        int z=0;
+        y=x;
+        z=0;
         while(y>0){
             z=10*z+y%10;
             y/=10;
         }
         if(z==x){
-            outpr<<z<<" ";
+            outpall<<z<<" ";
         }
 
     }
