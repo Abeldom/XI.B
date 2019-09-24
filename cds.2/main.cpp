5a2f2c9d4f4875d15e46c919c95e0293dca1f4af
Abeldom <47552057+Abeldom@users.noreply.github.com>
2019-09-24 07:51:47 +0300

    Add files via upload

--- cds.2/main.cpp
+++ cds.2/main.cpp
@@ -1,3 +1,8 @@
+///   A bac.txt állomány egy sorozatot tartalmaz legtöbb 106 különböző természetes számmal a [0,109] intervallumból. A számok egy-egy szóközzel vannak elválasztva.
+/// Határozza meg a két legnagyobb páros elemet a sorozatból, amelyek előtt csak három páratlan elem található. A meghatározott elemeket írassa ki a képernyőre
+/// szigorúan növekvő sorrendben, egy-egy szóközzel elválasztva, abban az esetben, ha a sorozatban nem létezik két ilyen elem, a képernyőre a Nu exista üzenetet írassa ki.
+/// A kért értékek meghatározására használjon hatékony algoritmust úgy a felhasznált memória, mint a futási idő szempontjából. Példa: ha az állomány a következő számokat tartalmazza
+
 #include <iostream>
 #include <fstream>
 
@@ -21,16 +26,13 @@ int main()
             if(x>par_max){
                 par_max2=par_max;
                 par_max=x;
-            }else{ if(x>par_max2){
+            }else if(x>par_max2)
                 par_max2=x;
-                }
-            }
+
+
         }
-        else{
-            if(szaml>3){
-                break;
-            }
-        }
+        if(szaml>3) break;
+
     }
 
     if(szaml<3||par_max2==-10000000){
