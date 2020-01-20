f305bf50ed3eb82cc2e1c5e5a7ac2f40d921324f
Abeldom <47552057+Abeldom@users.noreply.github.com>
2020-01-20 21:08:06 +0200

    modositottam a 6matrix fajlt

--- 6matrix.cpp
+++ 6matrix.cpp
@@ -8,48 +8,63 @@ using namespace std;
 int main()
 {
     srand(time(NULL));
-    int eso[100]={0};
-    int tavolsag[100]={0};
-    int kiirat[100][100]={0};
-    while(1>0){
-        for(int i=0;i<50;i++){
-            if(eso[i]==0 &&  tavolsag[i]<1){
-                eso[i]=rand()%6+1;
-                tavolsag[i]=rand()%10+3+eso[i];
-                kiirat[0][i]=1;
-            }else{
-                if(eso[i]>0){
-                    kiirat[0][i]=1;
+    int eso[100] = {0};
+    int tavolsag[100] = {0};
+    int kiirat[100][100] = {0};
+    while (1 > 0)
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            if (eso[i] == 0 && tavolsag[i] < 1)
+            {
+                eso[i] = rand() % 6 + 1;
+                tavolsag[i] = rand() % 10 + 3 + eso[i];
+                kiirat[0][i] = 1;
+            }
+            else
+            {
+                if (eso[i] > 0)
+                {
+                    kiirat[0][i] = 1;
                     eso[i]--;
-                }else if(eso[i]==0){
-                    kiirat[0][i]=0;
+                }
+                else if (eso[i] == 0)
+                {
+                    kiirat[0][i] = 0;
                 }
                 tavolsag[i]--;
             }
         }
-        for(int i=0;i<50;i++){
-            for(int j=0;j<50;j++){
-                if(kiirat[i][j]==0){
-                    cout<<" ";
-                }else{
-                    cout<<"|";
+        for (int i = 0; i < 50; i++)
+        {
+            for (int j = 0; j < 50; j++)
+            {
+                if (kiirat[i][j] == 0)
+                {
+                    cout << " ";
+                }
+                else
+                {
+                    cout << "|";
                 }
             }
-            cout<<endl;
+            cout << endl;
         }
 
-        for(int i=0;i<100;i++){
-            for(int j=48;j>=0;j--){
-                kiirat[j+1][i]=kiirat[j][i];
+        for (int i = 0; i < 100; i++)
+        {
+            for (int j = 48; j >= 0; j--)
+            {
+                kiirat[j + 1][i] = kiirat[j][i];
             }
-
         }
         Sleep(250);
-
-
     }
 
-
+    cout << "Itt valami uj kod jon" << endl;
+    /*
+        ....
+    */
 
     return 0;
 }
